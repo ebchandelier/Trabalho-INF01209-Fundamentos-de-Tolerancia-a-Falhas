2e1f69cf3f599038f3487cec78d4932c760d6caf
Eduardo BC <ebchandelier@gmail.com>
2018-04-23 21:55:40 -0300

    Made pi I/O Bound

--- src/pi/pi.c
+++ src/pi/pi.c
@@ -1,31 +1,32 @@
 #include<stdio.h>
-#include<math.h>
-#include<stdlib.h>
 
-int main(int argc, char *argv[]) {
-    double pi = 0.0;
-    for(double n =0.0; n<10000000.0; n++) {
-        pi += pow(-1.0, n) /  (2*n+1);    
+#define TAM 5000000
+
+FILE *file;
+
+double signal(double n) {
+    
+    if((int)n%2==0) {
+        
+        return 1.0;
     }
-    
-    FILE *file = fopen(argv[1], "w");
-    FILE *fileAux = fopen("aiaiai", "a+");
-
-
-    char output[55];
-    snprintf(output, 55, "%1.50f", pi*4);
-    
-    fputs(output, fileAux);
-    fputs(output, file);
-
-    fclose(fileAux);
-    fclose(file);
-    printf("SAVED =====> %1.50f <=====\n", pi*4);
-    // printf("SAVED ===> %s on ===> %s\n", output, argv[1]);
-    return 0;
+    return -1.0;
 }
 
-// # pi = 0
-// # for n in range(100000):
-// #     pi += (-1)**n /  (2*n+1)
-// # print(pi*4)
+int main(int argc, char *argv[]) {
+    
+    static double pi = 0.0;
+    
+    file = fopen(argv[1], "w+");
+    
+    for(double n =0.0; n<TAM; n++) {
+        
+        pi += signal(n) /  (2*n+1);    
+        
+        fprintf(file, "%1.70f...\n", pi*4);
+    }
+    
+    fclose(file);
+
+    return 0;
+}
